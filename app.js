1570a0efbb26f71b88c11b6fd38850c657032979
lldgm <wyd396@163.com>
2016-08-16 10:04:15 +0800

    init basice_node

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require("path");
+var ejs = require("ejs");
+var app = new express();
+
+// view engine
+app.set('views', path.join(__dirname, 'static'));
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+
+app.set('port', 3000);
+
+app.use(function(req, res, next) {
+    console.log(req.path, req.method, req.params, req.body, req.query);
+    //res.end('123456');
+    next();
+});
+app.use('/login', require('./routers/login'));
+
+module.exports = app;
