166cef6dd8876addd921e04b2b31de8e4691528e
guimaodai <wyyxdgm@163.com>
2016-08-17 22:51:36 +0800

    谁说我没写什么,git show [log id],瞧下

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ var app = new express();
 app.set('views', path.join(__dirname, 'static'));
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
-
+app.use(express.static(__dirname));
 app.set('port', 3000);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use(function(req, res, next) {
     console.log(req.path, req.method, req.params, req.body, req.query);
--- routers/login.js
+++ routers/login.js
@@ -4,13 +4,20 @@ router.get('/', function(req, res, next) {
     //res.end('这是一个login页面,请填写username,pass');
     res.render('login');
 });
-
+var TEST = true;
 router.post('/', function(req, res, next) {
     if (!req.body.username) return res.send('meiyou username');
     if (!req.body.pass) return res.send('meiyou pass');
+    if (TEST) return res.send(require("../data/login"));
+
     // resolve username pass
-   //res.end('您成功登录=====>>>>>>>username:' + req.body.username + ';pass:' + req.body.pass);
-   // res.redirect('../static/login.html');
+    httpClient.post(config.backend + "/login", req.body, function(json) {
+        res.send({
+            name: 'nimeide',
+            data: '您成功登录=====>>>>>>>username:' + req.body.username + ';pass:' + req.body.pass
+        });
+        res.redirect('/static/login.html');
+    });
 });
 
 
