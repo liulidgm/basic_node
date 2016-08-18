400343f6346abec71a6e4d70bcb0382de8f8f3d9
lldgm <wyd396@163.com>
2016-08-18 10:27:23 +0800

    ajax

--- routers/login.js
+++ routers/login.js
@@ -2,22 +2,25 @@ var router = require('express').Router();
 
 router.get('/', function(req, res, next) {
     //res.end('这是一个login页面,请填写username,pass');
-    res.render('login');
+    res.render('html/login');
 });
 var TEST = true;
 router.post('/', function(req, res, next) {
     if (!req.body.username) return res.send('meiyou username');
     if (!req.body.pass) return res.send('meiyou pass');
-    if (TEST) return res.send(require("../data/login"));
+    if (TEST) {
+        return res.send({flag:true});
+       // res.render('html/index');
+    }
 
-    // resolve username pass
-    httpClient.post(config.backend + "/login", req.body, function(json) {
+
+ /*   httpClient.post(config.backend + "/login", req.body, function(json) {
         res.send({
             name: 'nimeide',
             data: '您成功登录=====>>>>>>>username:' + req.body.username + ';pass:' + req.body.pass
         });
         res.redirect('/static/login.html');
-    });
+    });*/
 });
 
 
